1b5d25292a6a07f7e8db473d862eb44fe5ea8694
ChanceKoogler <kooglerc@gmail.com>
2021-01-24 20:17:43 -0500

    Update analysis_tools.py

--- analysis_tools.py
+++ analysis_tools.py
@@ -1,1 +1,11 @@
 import arcpy
+
+# Load and select building where FID = 4.
+arcpy.env.workspace = r"C:/Users/Chance/Documents/ArcGIS/OSU_data"
+in_features = "buildings.shp"
+out_feature_class = "D:/temp/buildingsSelect.shp"
+where = '"FID" = 4'
+
+
+# Execute
+arcpy.Select_analysis(in_features, out_feature_class, where)
